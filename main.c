468fbd693548244f7ea761dcc15b434b21300e29
Nam Yechan <chanmias0318@gmail.com>
2023-02-12 16:45:49 +0900

    Update struct

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yecnam <yecnam@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 15:04:45 by yecnam            #+#    #+#             */
-/*   Updated: 2023/02/12 14:56:09 by yecnam           ###   ########.fr       */
+/*   Updated: 2023/02/12 16:45:41 by yecnam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 
 int	info_init(int argc, char **argv, t_info *info)
 {
+	info->argc = argc;
+	info->argv = argv;
+	info->philo_num = atoi(argv[1]);
+	info->time_die = atoi(argv[2]);
+	info->time_eat = atoi(argv[3]);
+	info->time_sleep = atoi(argv[4]);
+	info->start_time = gettimeofday()
+	if (argc == 6)
+		info->must_eat = atoi(argv[5]);
 }
 
 int	thread_init(pthread_t **philo, int argc, char **argv)
